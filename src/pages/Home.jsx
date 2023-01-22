9183b3bc49fb01b5594938d3a9431f3323868159
333xAndy <jdlc.andy@gmail.com>
2023-01-21 17:26:08 -0800

    Segment button swtich

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,35 @@
 import {
+  IonCheckbox,
   IonCol,
   IonContent,
+  IonDatetime,
+  IonDatetimeButton,
   IonGrid,
   IonHeader,
   IonIcon,
   IonLabel,
+  IonModal,
   IonPage,
   IonRow,
   IonSegment,
   IonSegmentButton,
   IonToolbar,
   IonButton,
+  IonList,
+  IonListHeader,
+  IonItem,
+  IonAvatar,
+  IonTitle,
+  IonItemGroup,
+  IonItemDivider,
 } from "@ionic/react";
+import { Virtuoso } from 'react-virtuoso';
 import "./Home.css";
 import { add, cog } from "ionicons/icons";
+import { useState } from "react";
 
 function Home() {
+  const [sch, setSch] = useState(true)
   return (
     <IonPage>
       <IonHeader>
@@ -29,10 +43,10 @@ function Home() {
               </IonCol>
               <IonCol size="8">
                 <IonSegment value="default">
-                  <IonSegmentButton value="default">
+                  <IonSegmentButton onClick={()=> setSch(true)} value="default">
                     <IonLabel>Schedule</IonLabel>
                   </IonSegmentButton>
-                  <IonSegmentButton value="segment">
+                  <IonSegmentButton onClick={()=> setSch(false)} value="segment">
                     <IonLabel>My Drops</IonLabel>
                   </IonSegmentButton>
                 </IonSegment>
@@ -46,7 +60,72 @@ function Home() {
           </IonGrid>
         </IonToolbar>
       </IonHeader>
-      <IonContent class="ion-padding"></IonContent>
+
+      {sch ? (
+      <IonContent class="ion-padding">
+        <Virtuoso
+          style={{height: '100%'}}
+          totalCount={10}
+          itemContent={() => {
+            return (
+              <div style={{height:'60px'}}>
+                <IonItemGroup>
+                  <IonItemDivider>
+                    <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
+                    <IonModal keepContentsMounted={true}>
+                      <IonDatetime id="datetime"></IonDatetime>
+                    </IonModal>
+                  </IonItemDivider>
+                </IonItemGroup>
+
+                <IonItem>
+                  <IonAvatar slot="start">
+                    <img src="https://d3pq5rjvq8yvv1.cloudfront.net/catalog/product/cache/1/image/265x265/9df78eab33525d08d6e5fb8d27136e95/e/y/eye10.jpg" />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>[Eyedrop name here]</h2>
+                    <p>[For which eye]</p>
+                  </IonLabel>
+                  <IonItem>
+                    <IonCheckbox slot="end"></IonCheckbox>
+                    <p>[Time slot]</p>
+                  </IonItem>
+                </IonItem>
+              </div>
+            );
+          }}
+        />
+      </IonContent>
+      ): (<IonContent class="ion-padding">
+      <Virtuoso
+        style={{height: '100%'}}
+        totalCount={1}
+        itemContent={() => {
+          return (
+            <div style={{height:'60px'}}>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <img src="https://d3pq5rjvq8yvv1.cloudfront.net/catalog/product/cache/1/image/265x265/9df78eab33525d08d6e5fb8d27136e95/e/y/eye10.jpg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>[Your Eyedrop]</h2>
+                  <p>[For which eye]</p>
+                  <p>[How many times per day]</p>
+                </IonLabel>
+                <IonItem>
+                  <p>
+                  Starts:[This time] 
+                  <br></br>
+                  Ends:[This time]
+                  </p>
+                </IonItem>
+              </IonItem>
+            </div>
+          );
+        }}
+      />
+    </IonContent>)
+      }
     </IonPage>
   );
 }
